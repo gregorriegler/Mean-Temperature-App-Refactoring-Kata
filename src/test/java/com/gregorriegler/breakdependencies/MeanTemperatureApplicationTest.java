1f74fe94039001de79c5ece65482bef269846ee8
Gregor Riegler <gregor.riegler@ebcont.com>
2020-02-15 23:24:32 +0100

    test error case

--- src/test/java/com/gregorriegler/breakdependencies/MeanTemperatureApplicationTest.java
+++ src/test/java/com/gregorriegler/breakdependencies/MeanTemperatureApplicationTest.java
@@ -1,12 +1,20 @@
 package com.gregorriegler.breakdependencies;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class MeanTemperatureApplicationTest {
 
@@ -39,5 +47,26 @@ class MeanTemperatureApplicationTest {
             .contains(new Object[]{YearMonth.of(1969, 9), YearMonth.of(1969, 12), 2.5d})
             .contains(new Object[]{YearMonth.of(2000, 9), YearMonth.of(2000, 12), 2.5d})
             .contains(new Object[]{YearMonth.of(2019, 9), YearMonth.of(2019, 12), 2.5d});
+    }
+
+    @Test
+    void should_log_error() {
+        Appender appender = mock(Appender.class);
+        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(MeanTemperatureApplication.class);
+        logger.getLoggerContext().getLogger("ROOT").detachAndStopAllAppenders();
+        logger.addAppender(appender);
+
+        MeanTemperatureApplication application = new MeanTemperatureApplication() {
+            @Override
+            protected double[] fetchMeanList(YearMonth begin, YearMonth end) throws IOException {
+                throw new IOException("yep");
+            }
+        };
+
+        application.printMeans(YearMonth.of(2020, 2));
+
+        verify(appender, times(51)).doAppend(argThat(
+            o -> ((ILoggingEvent) o).getFormattedMessage().equals("an error occured"))
+        );
     }
 }
