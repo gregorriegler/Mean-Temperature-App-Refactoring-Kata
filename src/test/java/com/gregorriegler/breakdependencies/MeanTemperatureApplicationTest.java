0648fdd540c8bbc6bbcbe7991724160b7ec2f104
Gregor Riegler <gregor.riegler@ebcont.com>
2020-02-16 11:24:14 +0100

    refactor the tests

--- src/test/java/com/gregorriegler/breakdependencies/MeanTemperatureApplicationTest.java
+++ src/test/java/com/gregorriegler/breakdependencies/MeanTemperatureApplicationTest.java
@@ -18,32 +18,21 @@ import static org.mockito.Mockito.verify;
 
 class MeanTemperatureApplicationTest {
 
+    private final List<Object[]> fetchedMeans = new ArrayList<>();
+    private final List<Object[]> printInvocations = new ArrayList<>();
+
     @Test
-    void should_run() {
-        List<Object[]> ranges = new ArrayList<>();
-        List<Object[]> prints = new ArrayList<>();
-
-        MeanTemperatureApplication application = new MeanTemperatureApplication() {
-            @Override
-            protected double[] fetchMeanList(YearMonth begin, YearMonth end) {
-                ranges.add(new Object[]{begin, end});
-                return new double[]{1d, 2d, 3d, 4d};
-            }
-
-            @Override
-            protected void print(YearMonth begin, YearMonth end, double avg) {
-                prints.add(new Object[]{begin, end, avg});
-            }
-        };
+    void should_fetch_means_for_calculated_months_and_print_them() {
+        MeanTemperatureApplication application = new MeanTemperatureApplicationTss(new double[]{1d, 2d, 3d, 4d}, fetchedMeans, printInvocations);
 
         application.printMeans(YearMonth.of(2020, 2));
 
-        assertThat(ranges).hasSize(51)
+        assertThat(fetchedMeans).hasSize(51)
             .contains(new Object[]{YearMonth.of(1969, 9), YearMonth.of(1969, 12)})
             .contains(new Object[]{YearMonth.of(2000, 9), YearMonth.of(2000, 12)})
             .contains(new Object[]{YearMonth.of(2019, 9), YearMonth.of(2019, 12)});
 
-        assertThat(prints).hasSize(51)
+        assertThat(printInvocations).hasSize(51)
             .contains(new Object[]{YearMonth.of(1969, 9), YearMonth.of(1969, 12), 2.5d})
             .contains(new Object[]{YearMonth.of(2000, 9), YearMonth.of(2000, 12), 2.5d})
             .contains(new Object[]{YearMonth.of(2019, 9), YearMonth.of(2019, 12), 2.5d});
@@ -51,33 +40,16 @@ class MeanTemperatureApplicationTest {
 
     @Test
     void should_not_print_empty_result() {
-        List<Object[]> ranges = new ArrayList<>();
-        List<Object[]> prints = new ArrayList<>();
-
-        MeanTemperatureApplication application = new MeanTemperatureApplication() {
-            @Override
-            protected double[] fetchMeanList(YearMonth begin, YearMonth end) {
-                ranges.add(new Object[]{begin, end});
-                return new double[]{};
-            }
-
-            @Override
-            protected void print(YearMonth begin, YearMonth end, double avg) {
-                prints.add(new Object[]{begin, end, avg});
-            }
-        };
+        MeanTemperatureApplication application = new MeanTemperatureApplicationTss(new double[]{}, fetchedMeans, printInvocations);
 
         application.printMeans(YearMonth.of(2020, 2));
 
-        assertThat(prints).isEmpty();
+        assertThat(printInvocations).isEmpty();
     }
 
     @Test
     void should_log_error() {
-        Appender appender = mock(Appender.class);
-        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(MeanTemperatureApplication.class);
-        logger.getLoggerContext().getLogger("ROOT").detachAndStopAllAppenders();
-        logger.addAppender(appender);
+        Appender appender = mockLogAppender();
 
         MeanTemperatureApplication application = new MeanTemperatureApplication() {
             @Override
@@ -88,8 +60,42 @@ class MeanTemperatureApplicationTest {
 
         application.printMeans(YearMonth.of(2020, 2));
 
-        verify(appender, times(51)).doAppend(argThat(
-            o -> ((ILoggingEvent) o).getFormattedMessage().equals("an error occured"))
-        );
+        verify(appender, times(51)).doAppend(logMessage("an error occured"));
+    }
+
+    private Appender mockLogAppender() {
+        Appender appender = mock(Appender.class);
+        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(MeanTemperatureApplication.class);
+        logger.getLoggerContext().getLogger("ROOT").detachAndStopAllAppenders();
+        logger.addAppender(appender);
+        return appender;
+    }
+
+    private Object logMessage(String message) {
+        return argThat(
+            o -> ((ILoggingEvent) o).getFormattedMessage().equals(message));
+    }
+
+    private static class MeanTemperatureApplicationTss extends MeanTemperatureApplication {
+        private final List<Object[]> ranges;
+        private final double[] meanList;
+        private final List<Object[]> prints;
+
+        public MeanTemperatureApplicationTss(double[] meanList, List<Object[]> ranges, List<Object[]> prints) {
+            this.ranges = ranges;
+            this.meanList = meanList;
+            this.prints = prints;
+        }
+
+        @Override
+        protected double[] fetchMeanList(YearMonth begin, YearMonth end) {
+            ranges.add(new Object[]{begin, end});
+            return meanList;
+        }
+
+        @Override
+        protected void print(YearMonth begin, YearMonth end, double avg) {
+            prints.add(new Object[]{begin, end, avg});
+        }
     }
 }
